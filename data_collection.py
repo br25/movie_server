e51396c3bb75b99bd71a9f4a5d5071822e34e81f
br25 <md.ashrafulislamrobin12345@gmail.com>
2023-06-14 02:12:05 +0600

    get specific files

--- data_collection.py
+++ data_collection.py
@@ -2,36 +2,31 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.parse import urljoin
 
-# Prompt user for movie category selection
-movie_category = input("Enter the movie category (3D Movies, English Movies, Foreign Movies, IMDb Top-250 Movies, Tutorials): ")
-
-# Create the URL based on the selected movie category
-base_url = "http://172.16.50.7/SAM-FTP-2/"
-category_mapping = {
-    "3d movies": "3D Movies",
-    "english movies": "English Movies",
-    "foreign movies": "Foreign Language Movies",
-    "imdb top-250 movies": "IMDb Top-250 Movies",
-    "tutorials": "Tutorial"
-}
-category = category_mapping.get(movie_category.lower())
-if category is None:
-    print("Invalid movie category.")
-    exit()
-
-url = urljoin(base_url, category)
-
 # Fetch the web page
+url = "http://172.16.50.7/SAM-FTP-2/3D Movies"
 response = requests.get(url)
 html_content = response.text
 
 # Parse the HTML
 soup = BeautifulSoup(html_content, 'html.parser')
 
-# Extract the links for the selected movie category
+# Extract the links for the 3D movies
 links = soup.select('.fb-n a')
 for link in links:
     movie_name = link.text.strip()
     movie_url = link['href']
-    full_url = urljoin(base_url, movie_url)
-    print(f"Movie: {movie_name}, URL: {full_url}")
+    full_url = urljoin(url, movie_url)
+
+    # Get the movie file URL
+    response_full = requests.get(full_url)
+    soup_full = BeautifulSoup(response_full.text, 'html.parser')
+    file_links = soup_full.select('.fb-n a')
+
+    for file_link in file_links:
+        file_name = file_link.text.strip()
+        if file_name.endswith('.mkv'):
+            movie_file_url = urljoin(url, file_link['href'])
+            print(f"Movie: {movie_name}")
+            print(f"Movie File: {file_name}")
+            print(f"Movie File URL: {movie_file_url}")
+            break
